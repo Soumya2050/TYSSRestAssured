cbf5011f10b1624e6c92c8a02b9647f83c497afb
Soumya2050 <srjena2050@gmail.com>
2024-09-10 15:37:45 +0530

    recent

--- src/test/java/aaaaa/Sample_test.java
+++ src/test/java/aaaaa/Sample_test.java
@@ -4,12 +4,6 @@ public class Sample_test {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("test git desktop ");
-		System.out.println("cosnflict statement");
-		System.out.println("somu jeena");
-		
-		
-		
-		
+		System.out.println("Soumyaranjan Jena");
 	}
 }
