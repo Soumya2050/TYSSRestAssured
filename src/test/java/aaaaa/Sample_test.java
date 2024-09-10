a580379e5a6c7e755b00cece8edeff9d950def28
Soumya2050 <srjena2050@gmail.com>
2024-09-10 15:43:55 +0530

    sghhfd

--- src/test/java/aaaaa/Sample_test.java
+++ src/test/java/aaaaa/Sample_test.java
@@ -2,9 +2,9 @@ package aaaaa;
 
 public class Sample_test {
 public static void main(String[] args) {
-	System.out.println("test git desktop ");
-	System.out.println("cosnflict statement");
-	System.out.println("somu jeena");
-	System.out.println("modcommit");
+	
+	
+	
+	
 }
 }
